3b81a82cdd8a61a20cad41edd173d58c7ea06caf
Dmitrii <dmitrii1997033@gmail.com>
2024-04-06 12:18:00 +0300

    changed connection string to MongoDB

--- back/config/connectDB.js
+++ back/config/connectDB.js
@@ -3,6 +3,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URI);
     // "mongodb://mongodb:27017/users"
+    //  mongodb://localhost:27017/api
   } catch (error) {
     console.error(error);
   }
